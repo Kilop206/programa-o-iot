bf634536f98db85aab998c0bebcba3d3dfdda1c9
Kilop206 <guilhermextra118@gmail.com>
2026-08-12 11:01:56 -0300

    feat: loop

--- teste-compilador/decisao.cpp
+++ teste-compilador/decisao.cpp
@@ -1,19 +1,33 @@
 #include <iostream>
+#include <string>
 
 int main() {
 
-	int idade;
+	std::string nome;
+	char resposta;
 
-	std::cout << "Insira sua idade: ";
-	std::cin >> idade;
+	std::cout << "Qual o seu nome: ";
+	std::cin >> nome;
 
-	if (idade >= 18) {
-		std::cout << "Você é maior de idade" << std::endl;
-	} else if (idade <= 12) {
-		std::cout << "Você é uma criança" << std::endl;
-	} else {
-		std::cout << "Você é um adolescente" << std::endl;
-	}
+	do {
+
+		int idade;
+
+		std::cout << "Insira sua idade: ";
+		std::cin >> idade;
+
+		if (idade >= 18) {
+			std::cout << "Você é maior de idade" << std::endl;
+		} else if (idade <= 12) {
+			std::cout << "Você é uma criança" << std::endl;
+		} else {
+			std::cout << "Você é um adolescente" << std::endl;
+		}
+
+		std::cout << "Deseja continuar?\nS - Sim\nN - Não";
+		std::cin >> resposta;
+
+	} while (resposta == 'S' || resposta == 's');
 
 	return 0;
 }
